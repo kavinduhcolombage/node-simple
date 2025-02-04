00a30f87daaea9f63ed0bb1f9f869425bf287914
Kavindu Hansana <hansana2024@gmail.com>
2025-02-05 00:18:27 +0530

    Change server port from 3001 to 3000

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log('Server running at http://localhost:3001/');
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
 });
